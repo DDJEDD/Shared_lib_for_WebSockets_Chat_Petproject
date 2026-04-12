b70c706fb10de4728cc8218346ff3e76c10cd67b
я хз что писать <149868058+DDJEDD@users.noreply.github.com>
2026-04-12 14:15:20 +0300

    redis

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="petproject_shared",
-      version="1.1.4",
+      version="1.2.0",
       packages=find_packages(),
       install_requires=["PyJWT","sqlalchemy", "redis" ],
       python_requires=">=3.12")
