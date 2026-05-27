b9827088da53957ffca6d5a132135a985e68216b
я хз что писать <149868058+DDJEDD@users.noreply.github.com>
2026-05-27 22:42:07 +0300

    redis refactor

--- petproject_shared/redis.py
+++ petproject_shared/redis.py
@@ -1,14 +1,13 @@
-import redis.asyncio as redis
-
 class RedisService:
     def __init__(self, redis_service):
-
         self.r = redis_service
 
     async def set_user(self, user_id: int, username: str, login: str = None):
         data = {"username": username}
         if login:
             data["login"] = login
+            await self.r.set(f"login:{login}", user_id)
+
         await self.r.set(f"username:{username}", user_id)
         await self.r.hset(f"user:{user_id}", mapping=data)
 
@@ -16,19 +15,16 @@ class RedisService:
         return await self.r.exists(f"user:{user_id}")
 
     async def get_user(self, user_id: int):
-        data = await self.r.hgetall(f"user:{user_id}")
-        return data
+        return await self.r.hgetall(f"user:{user_id}")
+
     async def get_id_by_login(self, login: str):
         return await self.r.get(f"login:{login}")
 
     async def update_username(self, user_id: int, old_username: str, new_username: str):
         if old_username:
             await self.r.delete(f"username:{old_username}")
+
         await self.r.set(f"username:{new_username}", user_id)
 
         if await self.r.exists(f"user:{user_id}"):
-
             await self.r.hset(f"user:{user_id}", "username", new_username)
-        else:
-
-            pass
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="petproject_shared",
-      version="1.2.8",
+      version="1.3.0",
       packages=find_packages(),
       install_requires=["PyJWT","sqlalchemy", "redis" ],
       python_requires=">=3.12")
